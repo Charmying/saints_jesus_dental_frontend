2386755f3fe549a2dc34c18ec65e68d746fc2f79
Charmy <charmytseng0118@gmail.com>
2025-05-25 17:53:27 +0800

    整理 code

--- src/components/CarouselBanner.tsx
+++ src/components/CarouselBanner.tsx
@@ -15,54 +15,33 @@ interface CarouselBannerProps {
 }
 
 /** banner 輪播項目元件 */
-const Slide = ({
-  slide,
-  isActive,
-  minHeight,
-}: {
-  slide: SlideItem;
-  isActive: boolean;
-  minHeight: string;
-}) => {
+const Slide = ({ slide, isActive, minHeight }: { slide: SlideItem; isActive: boolean; minHeight: string; }) => {
   return (
     <div
-      className={`absolute top-0 left-0 w-full h-full transition-opacity duration-1000 ease-in-out ${isActive ? 'opacity-100 z-10' : 'opacity-0 z-0 pointer-events-none'}`}
-      style={{
-        backgroundImage: `url(${slide.bgImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        minHeight,
-      }}
+      className={ `w-full h-full absolute top-0 left-0 transition-opacity duration-1000 ease-in-out ${ isActive ? 'opacity-100 z-10' : 'opacity-0 z-0 pointer-events-none' }` }
+      style={{ backgroundImage: `url(${slide.bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight, }}
     >
-      {/* 半透明遮罩效果 */}
-      <div
-        className="absolute inset-0"
-        style={{backgroundImage: 'linear-gradient(90deg, 0%, transparent 100%)'}}
-      />
+      { /* 半透明遮罩效果 */ }
+      <div className="absolute inset-0" style={{ backgroundImage: 'linear-gradient(90deg, 0%, transparent 100%)' }} />
 
-      <div className="relative flex w-full h-full items-end px-7 md:px-10">
-        <div className="w-full md:w-2/5 mb-10">
+      <div className="w-full h-full flex items-end px-7 relative md:px-10">
+        <div className="w-full mb-10 md:w-2/5">
           <p className="text-white text-center md:hidden" dangerouslySetInnerHTML={{ __html: slide.titleMobile }} />
-          <p className="hidden md:block text-white text-2xl">{slide.titleDesktop}</p>
+          <p className="hidden text-white md:block">{slide.titleDesktop}</p>
         </div>
 
-        {/* 中間 scroll 圖示，只在電腦版顯示 */}
-        <div
-          className="w-1/5 hidden md:block mb-10 cursor-pointer"
+        { /* 中間 scroll 圖示，只在電腦版顯示 */ }
+        <div className="w-1/5 mb-10 hidden cursor-pointer md:block"
           onClick={() => {
             const main = document.querySelector('main');
             main?.scrollIntoView({ behavior: 'smooth' });
           }}
         >
-          <img
-            src="https://hsin-ho.tw/wp-content/uploads/2023/08/SCROLL.svg"
-            alt="Scroll"
-            className="m-auto"
-          />
+          <img src="https://hsin-ho.tw/wp-content/uploads/2023/08/SCROLL.svg" alt="Scroll" className="m-auto" />
         </div>
 
-        {/* 右側空白佔位（電腦版） */}
-        {/* <div className="w-2/5 hidden md:block" /> */}
+        { /* 右側空白佔位（電腦版） */ }
+        { /* <div className="w-2/5 hidden md:block" /> */ }
       </div>
     </div>
   );
@@ -80,8 +59,8 @@ export const CarouselBanner = ({slides, interval = 2500, minHeight = '70vh'}: Ca
   }, [slides.length, interval]);
 
   return (
-    <section className="w-full mb-24 md:mb-40 overflow-hidden">
-      <div className="relative w-full" style={{minHeight}}>
+    <section className="w-full mb-24 overflow-hidden md:mb-40">
+      <div className="w-full relative" style={{ minHeight }}>
         {slides.map((slide, index) => (
           <Slide key={index} slide={slide} isActive={index === currentSlide} minHeight={minHeight} />
         ))}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,35 +29,35 @@ function Header() {
 
   return (
     <>
-      {/* 佔位標籤 */}
+      { /* 佔位標籤 */ }
       <div className="h-[60px]" />
 
-      <header className={`bg-gray-900 w-full fixed top-0 left-0 z-50 shadow-md transition-transform duration-300 ${hidden ? '-translate-y-full' : 'translate-y-0'}`}>
-        <nav className="px-8 flex justify-between items-center h-[60px] relative">
-          {/* logo */}
-          <div className={`md:static absolute left-1/2 -translate-x-1/2 md:left-auto md:translate-x-0`}>
+      <header className={`w-full bg-gray-900 fixed top-0 left-0 z-50 shadow-md transition-transform duration-300 ${hidden ? '-translate-y-full' : 'translate-y-0'}`}>
+        <nav className="h-[60px] px-8 flex justify-between items-center relative">
+          { /* logo */ }
+          <div className={`absolute left-1/2 -translate-x-1/2 md:static md:left-auto md:translate-x-0`}>
             <img src="/public/images/logo_white.png" alt="header logo" className="h-[40px] md:h-[50px]" />
           </div>
 
-          {/* 漢堡按鈕 */}
-          <div className="md:hidden absolute right-4 flex items-center z-10">
+          { /* 漢堡按鈕 */ }
+          <div className="flex items-center absolute right-4 z-10 md:hidden">
             <button onClick={toggleMenu} className="relative w-[24px] h-[24px] z-10">
-              <span className={`absolute top-1/2 left-0 w-full h-[2px] bg-white transform transition-all duration-300 ${menuOpen ? 'rotate-45' : '-translate-y-2'}`}></span>
-              <span className={`absolute top-1/2 left-0 w-full h-[2px] bg-white transition-all duration-300 ${menuOpen ? 'opacity-0' : ''}`}></span>
-              <span className={`absolute top-1/2 left-0 w-full h-[2px] bg-white transform transition-all duration-300 ${menuOpen ? '-rotate-45' : 'translate-y-2'}`}></span>
+              <span className={`w-full h-[2px] bg-white absolute top-1/2 left-0 transform transition-all duration-300 ${menuOpen ? 'rotate-45' : '-translate-y-2'}`}></span>
+              <span className={`w-full h-[2px] bg-white absolute top-1/2 left-0 transition-all duration-300 ${menuOpen ? 'opacity-0' : ''}`}></span>
+              <span className={`w-full h-[2px] bg-white absolute top-1/2 left-0 transform transition-all duration-300 ${menuOpen ? '-rotate-45' : 'translate-y-2'}`}></span>
             </button>
           </div>
 
-          {/* 桌機版選單 */}
-          <ul className="hidden md:flex gap-4 text-sm text-white">
+          { /* 桌機版選單 */ }
+          <ul className="hidden text-white md:flex gap-4">
             <li><Link to="/">首頁</Link></li>
             <li><Link to="/about">關於我們</Link></li>
           </ul>
         </nav>
 
-        {/* 手機版選單 */}
-        <div className={`md:hidden bg-gray-900 transition-all duration-500 overflow-hidden ${menuOpen ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'}`}>
-          <ul className="flex flex-col text-white text-center py-4 gap-2">
+        { /* 手機版選單 */ }
+        <div className={`bg-gray-900 transition-all duration-500 overflow-hidden md:hidden ${menuOpen ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'}`}>
+          <ul className="py-4 flex flex-col gap-2 text-white text-center">
             <li><Link to="/" onClick={() => setMenuOpen(false)}>首頁</Link></li>
             <li><Link to="/about" onClick={() => setMenuOpen(false)}>關於我們</Link></li>
           </ul>
