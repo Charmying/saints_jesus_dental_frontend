d7c5d378fcd2de6d9967ebeab446933ef5886bb9
Charmy <charmytseng0118@gmail.com>
2025-06-13 22:00:17 +0800

    Tailwind RWD 斷點調整

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ export default {
   ],
   theme: {
     extend: {
+    screens: {
+      'sm': '499px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1200px',
+      '2xl': '1400px',
+      '3xl': '1920px',
+    },
       colors: {
         white: '#FFFFF0',
         black: '#00000F',
